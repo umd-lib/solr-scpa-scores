39171d26e302e27710c6f9cfe0888b698f0fb7a1
Ben Wallberg <wallberg@umd.edu>
2020-09-01 09:39:12 -0400

    LIBITD-1669. Add id column validation
    Also some minor formatting fixes
    
    https://issues.umd.edu/browse/LIBITD-1669

--- scripts/cleanup.py
+++ scripts/cleanup.py
@@ -4,53 +4,86 @@ import csv
 import sys
 import re
 
-fieldnames = ['id','composer','title','imprint','instrumentation','collation',
-              'additional_info','collection','call_number','duration',
-              'solo_difficulty','difficulty','pages','ensemble_description',
-              'ensemble_size','fair_use','special']
+fieldnames = ['id', 'composer', 'title', 'imprint', 'instrumentation',
+              'collation', 'additional_info', 'collection', 'call_number',
+              'duration', 'solo_difficulty', 'difficulty', 'pages',
+              'ensemble_description', 'ensemble_size', 'fair_use', 'special']
 
 p_multipipe = re.compile(r'\|+')
 p_multispace = re.compile(r' *\| *')
-p_trailingpipe = re.compile(f'\|$')
+p_trailingpipe = re.compile(r'\|$')
 
-# Open input file for reading and output file for writing
-with open(sys.argv[1]) as ihandle, open(sys.argv[2], 'w') as ohandle:
-    reader = csv.DictReader(ihandle, fieldnames=fieldnames)
-    writer = csv.DictWriter(ohandle, fieldnames=fieldnames)
-    writer.writeheader()
+if __name__ == '__main__':
+    def warn(msg):
+        ''' Print validation warning message. '''
 
-    for n,row in enumerate(reader, start=1):
+        print(f'row#={rownum}, id={id}, {msg}')
 
-        for field in fieldnames:
-            if field == 'id':
-                # Zero pad id to 8 digits
-                row['id'] = f"{int(row['id']):08}"
+    # Open input file for reading and output file for writing
+    with open(sys.argv[1]) as ihandle, open(sys.argv[2], 'w') as ohandle:
+        reader = csv.DictReader(ihandle, fieldnames=fieldnames)
+        writer = csv.DictWriter(ohandle, fieldnames=fieldnames)
+        writer.writeheader()
 
-            else:
-                new_value = row[field]
+        is_valid = True
+        all_ids = set()
 
-                # Replace Control character K (represents multiple values)
-                # with PIPE
-                new_value = new_value.replace('\v', '|')
+        for rownum, row in enumerate(reader, start=1):
 
-                # Replace multiple PIPEs with single PIPE
-                # (To get rid of empty values in a multivalued field
-                new_value = p_multipipe.sub('|', new_value)
+            for field in fieldnames:
+                if field == 'id':
+                    id = row['id']
 
-                # # Trim extra spaces between values in a multivalued field
-                new_value = p_multispace.sub('|', new_value)
+                    try:
+                        id = int(id)
+                        if id < 1:
+                            raise ValueError(f'not a positive integer: {id}')
 
-                # Trim extra space between fields
-                new_value = new_value.strip()
+                        # Zero pad id to 8 digits
+                        id = f"{int(row['id']):08}"
 
-                # Remove trailing PIPE in a field
-                new_value = p_trailingpipe.sub('', new_value)
+                        if id in all_ids:
+                            raise ValueError(f'not unique: {id}')
 
-                # Remove in-between spaces in instrumentation
-                if field == 'instrumentation':
-                    new_value = ','.join(e.strip() for e in
-                                         new_value.split(','))
+                        all_ids.add(id)
+                        row['id'] = id
 
-                row[field] = new_value
+                    except ValueError as err:
+                        id = "?"
+                        warn(f'id column: {err}')
+                        is_valid = False
 
-        writer.writerow(row)
+                else:
+                    new_value = row[field]
+
+                    # Replace Control character K (represents multiple values)
+                    # with PIPE
+                    new_value = new_value.replace('\v', '|')
+
+                    # Replace multiple PIPEs with single PIPE
+                    # (To get rid of empty values in a multivalued field
+                    new_value = p_multipipe.sub('|', new_value)
+
+                    # # Trim extra spaces between values in a multivalued field
+                    new_value = p_multispace.sub('|', new_value)
+
+                    # Trim extra space between fields
+                    new_value = new_value.strip()
+
+                    # Remove trailing PIPE in a field
+                    new_value = p_trailingpipe.sub('', new_value)
+
+                    # Remove in-between spaces in instrumentation
+                    if field == 'instrumentation':
+                        new_value = ','.join(e.strip() for e in
+                                             new_value.split(','))
+
+                    row[field] = new_value
+
+            writer.writerow(row)
+
+    # Exit with error code if validation failed
+    if is_valid:
+        sys.exit(0)
+    else:
+        sys.exit(1)
