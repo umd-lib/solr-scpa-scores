f5cc67ece7e4154fcdee1c31c8d4ec9546e9422a
Ben Wallberg <wallberg@umd.edu>
2020-09-04 07:27:22 -0400

    LIBITD-1669. Move instrumentation mapping to cleanup.py
    Removed update-scripts.js altogether.  All validation,
    cleanup, and updates are consolidated in scripts/cleanup.py
    
    TODO: complete the instrumentation functionality
    TODO: create and pass all instrumentation tests
    
    https://issues.umd.edu/browse/LIBITD-1669

--- scripts/cleanup.py
+++ scripts/cleanup.py
@@ -3,19 +3,24 @@
 import csv
 import sys
 import re
+from unittest import TestLoader, TextTestRunner, TestCase
 
 # Process a SCPA Scores Collection CSV file:
 #
 # - validate the input data and report problems
 # - add the header
 # - cleanup the input data
-# - generate new columns for indexing
+# - generate new fields for indexing
 
 fieldnames = ['id', 'composer', 'title', 'imprint', 'instrumentation',
               'collation', 'additional_info', 'collection', 'call_number',
               'duration', 'solo_difficulty', 'difficulty', 'pages',
-              'ensemble_description', 'ensemble_size', 'fair_use', 'special',
-              'collection_dictionary', 'collection_sorted_dictionary']
+              'ensemble_description', 'ensemble_size', 'fair_use', 'special']
+
+new_fieldnames = ['collection_dictionary', 'collection_sorted_dictionary',
+                  'instrumentation_dictionary',
+                  'instrumentation_dictionary_full',
+                  'instrumentation_dictionary_full_with_alt']
 
 collection_dict = {
     "ICA": "001::International Clarinet Association (ICA) Score Collection",
@@ -38,28 +43,318 @@ collection_dict = {
     "VdGSA": "018::Viola da Gamba Society of America Archives"
 }
 
+# Instrument code to label map
+inst_dict = {
+    "any": "any instrument",
+    "any-bb": "any b-flat instrument",
+    "any-bc": "any bass clef instrument",
+    "any-eb": "any e-flat instrument",
+    "any-melody": "any melody instrument",
+    "any-tc": "any treble clef instrument",
+    "basso-continuo": "basso continuo",
+    "ondes-martenot": "ondes martenot",
+    "org": "organ",
+    "pno": "piano",
+    "pno-prep": "prepared piano",
+    "pno-toy": "toy piano",
+    "synth": "synthesizer",
+    "cymbal-finger": "finger cymbal",
+    "drum-bass": "bass drum",
+    "drum-hand": "hand drum",
+    "drum-snr": "snare drum",
+    "drum-st": "drum set",
+    "glock": "glockenspiel",
+    "mrmba": "marimba",
+    "perc": "percussion",
+    "rhythm": "rhythm section",
+    "tamb": "tambourine",
+    "timp": "timpani",
+    "xylo": "xylophone",
+    "bsn": "bassoon",
+    "cl": "clarinet",
+    "cl-a": "a clarinet",
+    "cl-alt": "alto clarinet",
+    "cl-alt-f": "f alto clarinet",
+    "cl-bb": "b-flat clarinet",
+    "cl-bs": "bass clarinet",
+    "cl-c": "c clarinet",
+    "cl-choir": "clarinet choir",
+    "cl-ctralt": "contralto clarinet",
+    "cl-ctralt-bb": "b-flat contralto clarinet",
+    "cl-ctralt-eb": "e-flat contralto clarinet",
+    "cl-ctrbs": "contrabass clarinet",
+    "cl-ctrbs-bb": "b-flat contrabass clarinet",
+    "cl-ctrbs-eb": "e-flat contrabass clarinet",
+    "cl-eb": "e-flat clarinet",
+    "cl-eb-alt": "alto e-flat clarinet",
+    "cl-sop": "soprano clarinet",
+    "cl-sop-ab": "a-flat soprano clarinet",
+    "cl-sop-bb": "b-flat soprano clarinet",
+    "cl-sop-eb": "e-flat soprano clarinet",
+    "ctrbs": "contrabassoon",
+    "ctrbs-bb": "e-flat contrabassoon",
+    "ctrbs-eb": "b-flat contrabassoon",
+    "fl": "flute",
+    "fl-a": "alto flute",
+    "fl-bs": "bass flute",
+    "hrn-eng": "english horn",
+    "ob": "oboe",
+    "picc": "piccolo",
+    "rec": "recorder",
+    "rec-alt": "alto recorder",
+    "rec-bs": "bass recorder",
+    "rec-sop": "soprano recorder",
+    "rec-ten": "tenor recorder",
+    "sax": "saxophone",
+    "sax-alt": "alto saxophone",
+    "sax-alt-eb": "alto e-flat saxophone",
+    "sax-b": "bass saxophone",
+    "sax-bar-eb": "baritone e-flat saxophone",
+    "sax-bari": "baritone saxophone",
+    "sax-bb": "b-flat saxophone",
+    "sax-bb-sop": "b-flat soprano saxophone",
+    "sax-bs": "bass saxophone",
+    "sax-c": "c saxophone",
+    "sax-eb": "e-flat saxophone",
+    "sax-eb-alt": "e-flat alto saxophone",
+    "sax-f-mezsop": "f mezzo-soprano saxophone",
+    "sax-sop": "soprano saxophone",
+    "sax-ten": "tenor saxophone",
+    "woodwinds": "woodwind instruments",
+    "woodwinds-choir": "woodwind choir",
+    "woodwinds-orch": "wind orchestra",
+    "bari": "baritone",
+    "bari-bc": "bass clef baritone",
+    "bari-tc": "treble clef baritone",
+    "brass": "brass instruments",
+    "cor": "cornet",
+    "euph": "euphonium",
+    "hrn": "horn",
+    "hrn-a": "a horn",
+    "hrn-basst": "basset horn",
+    "hrn-bb": "b-flat horn",
+    "hrn-c": "c horn",
+    "hrn-d": "d horn",
+    "hrn-e": "e horn",
+    "hrn-eb": "e-flat horn",
+    "tba-bb": "b-flat tuba",
+    "tba-bs": "bass tuba",
+    "tba-c": "c tuba",
+    "tba-eb": "e-flat tuba",
+    "tba-f": "f tuba",
+    "tba-tc": "treble clef tuba",
+    "tbn": "trombone",
+    "tbn-alt": "alto trombone",
+    "tbn-bs": "bass trombone",
+    "tbn-f": "trombone with f attachment",
+    "tbn-ten": "tenor trombone",
+    "tpt": "trumpet",
+    "tpt-a": "a trumpet",
+    "tpt-bb": "b-flat trumpet",
+    "tpt-c": "c trumpet",
+    "tpt-d": "d trumpet",
+    "tpt-eb": "e-flat trumpet",
+    "tpt-f": "f trumpet",
+    "tpt-picc": "piccolo trumpet",
+    "gtr": "guitar",
+    "strings": "string instruments",
+    "strings-quar": "string quartet",
+    "vcl": "cello (violincello)",
+    "vla": "viola",
+    "vla-dg": "viola da gamba",
+    "vln": "violin",
+    "narr": "narrator",
+    "satb": "choir",
+    "voice-alt": "alto voice",
+    "voice-bar": "baritone voice",
+    "voice-bass": "bass voice",
+    "voice-mez-sop": "mezzo soprano voice",
+    "voice-sop": "soprano voice",
+    "voice-ten": "tenor voice",
+    "band-brass": "brass band",
+    "band-concert": "concert band",
+    "band-marching": "marching band",
+    "band-symph": "symphonic band",
+    "jazz": "jazz ensemble",
+    "orch": "orchestra",
+    "orch-chamber": "chamber orchestra",
+    "orch-polka": "polka orchestra",
+    "orch-string": "string orchestra",
+    "accord": "accordion",
+    "conch": "conch shell",
+    "gtr-bs": "bass guitar",
+    "penny-whistle": "penny whistle",
+    "opt": "optional",
+    "ens": "ensemble"
+}
+
 p_multipipe = re.compile(r'\|+')
 p_multispace = re.compile(r' *\| *')
 p_trailingpipe = re.compile(r'\|$')
+p_inst = re.compile(r'([\w-]+?) *\( *([0-9]+|ens|opt) *\)')
 
-if __name__ == '__main__':
+
+def inst_sort_key(obj):
+    '''
+    Sort instruments by:
+    0 - without alternatives, eg, "pno", with a numeric count
+    1 - alternatives with counts other than 1
+    2 - alternatives with all counts of 1
+    3 - without alternatives, with count of ensemble or optional
+    '''
+
+    if len(obj) == 1 and isinstance(obj[0][1], int):
+        return 0
+    elif len(obj) > 1 and any(s[1] != 1 for s in obj):
+        return 1
+    elif len(obj) > 1:
+        return 2
+    else:
+        return 3
+
+
+def parse_inst(inst):
+    '''
+    Parse a single instrument code with optional count in parens, eg:
+      code
+      code(1)
+      code(10)
+      code(opt)
+      code(ens)
+
+    Return a tuple with (code, count)
+    '''
+
+    match = p_inst.match(inst)
+    if match:
+        code = match.group(1)
+        try:
+            count = match.group(2)
+            if count == 'opt':
+                count = 'optional'
+            elif count == 'ens':
+                count = 'ensemble'
+            else:
+                count = int(count)
+        except:
+            count = 1
+    else:
+        code, count = inst, 1
+
+    return (code, count)
+
+
+def parse_inst_list(value):
+    '''
+    Parse the list of instruments w/ alternatives in the instrumentation field.
+    '''
+
+    parsed = [[parse_inst(i.strip()) for i in alt.strip().split('|')]
+              for alt in value.split(',')]
+
+    # Sort the instrument list
+    return sorted(parsed, key=inst_sort_key)
+
+def get_inst_dict(inst):
+    ''' Get the full name of the inst code from inst_dict. '''
+
+    if inst in inst_dict:
+        return inst_dict[inst]
+    else:
+        return inst
+
+
+def get_name_with_count(name, count):
+    ''' Join the name with count for display. '''
+
+    if isinstance(count, int):
+        return f'{count} {name}'
+    else:
+        return f'{name} [{count}]'
+
+
+def get_idf(name, count, name_with_count):
+    ''' Get the value for instrumentation_dictionary_full. '''
+
+    if isinstance(count, int):
+        count = f'{count:03}'
+
+    return f'{name}{count}::{name_with_count}'
+
+
+def get_instrument_fields(new_value):
+    '''
+    Parse the instrumentation field and build new fields:
+
+      instrumentation_dictionary - faceting
+      instrumentation_dictionary_full - faceting (dependent)
+      instrumentation_dictionary_full_with_alt - display
+    '''
+
+    id, idf, idfwa = [], [], []
+
+    # Parse the instrument list, splitting on ',' and their
+    # alternatives on '|'.
+    inst_values = parse_inst_list(new_value)
+
+    # Track cumulative counts per instrument
+    counts = {}
+
+    # Iterate over the instrument list
+    for alt in inst_values:
+
+        display_name = []
+
+        # Iterate over the alternatives
+        for inst, count in alt:
+
+            name = get_inst_dict(inst)
+            name_with_count = get_name_with_count(name, count)
+
+            display_name.append(name_with_count)
+
+            if name not in counts:
+                # Add the id facet (only once per instrument)
+                id.append(name)
+                counts[name] = count
+            else:
+                if isinstance(counts[name], int) and isinstance(count, int):
+                    counts[name] += count
+                else:
+                    counts[name] = count
+
+            name_with_idf_count = get_name_with_count(name, counts[name])
+
+            idf.append(get_idf(name, counts[name], name_with_idf_count))
+
+        # Add the display name
+        idfwa.append(' OR '.join(display_name))
+
+    return id, idf, idfwa
+
+
+def cleanup():
+    ''' Main loop for cleanup and validation. '''
+
     def warn(field, msg):
         ''' Print validation warning message. '''
-
         print(f'row#={rownum}, id={id}, {field} field {msg}')
 
     # Open input file for reading and output file for writing
     with open(sys.argv[1]) as ihandle, open(sys.argv[2], 'w') as ohandle:
         reader = csv.DictReader(ihandle, fieldnames=fieldnames)
-        writer = csv.DictWriter(ohandle, fieldnames=fieldnames)
+        writer = csv.DictWriter(ohandle, fieldnames=fieldnames+new_fieldnames)
         writer.writeheader()
 
         is_valid = True
         all_ids = set()
 
+        # Iterate over the input rows
         for rownum, row in enumerate(reader, start=1):
 
+            # Iterate over the fields in each row
             for field in fieldnames:
+
                 new_value = row[field]
 
                 if field == 'id':
@@ -94,7 +389,7 @@ if __name__ == '__main__':
                     # (To get rid of empty values in a multivalued field
                     new_value = p_multipipe.sub('|', new_value)
 
-                    # # Trim extra spaces between values in a multivalued field
+                    # Trim extra spaces between values in a multivalued field
                     new_value = p_multispace.sub('|', new_value)
 
                     # Trim extra space between fields
@@ -124,16 +419,134 @@ if __name__ == '__main__':
                     row['collection_sorted_dictionary'] = csd
 
                 if field == 'instrumentation':
-                    # Remove in-between spaces in instrumentation
-                    new_value = ','.join(e.strip() for e in
-                                            new_value.split(','))
+
+                    if new_value != "":
+
+                        field_id, field_idf, field_idfwa = \
+                            get_instrument_fields(new_value)
+
+                        row['instrumentation_dictionary'] = \
+                            ','.join(field_id)
+
+                        row['instrumentation_dictionary_full'] = \
+                            ','.join(field_idf)
+
+                        row['instrumentation_dictionary_full_with_alt'] = \
+                            ','.join(field_idfwa)
+
 
                 row[field] = new_value
 
             writer.writerow(row)
 
     # Exit with error code if validation failed
-    if is_valid:
-        sys.exit(0)
-    else:
+    # TODO: enable this once we want to enforce validation
+    #
+    # if is_valid:
+    #     sys.exit(0)
+    # else:
+    #     sys.exit(1)
+
+class Test(TestCase):
+
+    def test_parse_inst(self):
+        self.assertEqual(parse_inst('foo'), ('foo', 1))
+        self.assertEqual(parse_inst('foo_bar'), ('foo_bar', 1))
+        self.assertEqual(parse_inst('foo2'), ('foo2', 1))
+        self.assertEqual(parse_inst('cl-bb (3)'), ('cl-bb', 3))
+
+        self.assertEqual(parse_inst('foo (2)'), ('foo', 2))
+        self.assertEqual(parse_inst('foo (10)'), ('foo', 10))
+        self.assertEqual(parse_inst('foo ( opt )'), ('foo', 'optional'))
+        self.assertEqual(parse_inst('foo(ens) '), ('foo', 'ensemble'))
+
+        self.assertEqual(parse_inst('foo (bad)'), ('foo (bad)', 1))
+        self.assertEqual(parse_inst('foo bad'), ('foo bad', 1))
+
+    def test_parse_inst_list(self):
+        self.assertEqual(parse_inst_list('cl (2)'),
+                         [[('cl', 2)]])
+
+        self.assertEqual(parse_inst_list('cl, bsn'),
+                         [[('cl', 1)], [('bsn', 1)]])
+
+        self.assertEqual(parse_inst_list('cl-bb (4)|cl-bb (2), cl-alt, cl-bs'),
+                         [[('cl-alt', 1)], [('cl-bs', 1)],
+                          [('cl-bb', 4), ('cl-bb', 2)]])
+
+        self.assertEqual(parse_inst_list('pno,cl|vln,cl(2)|cl(3)'),
+                         [[('pno', 1)], [('cl', 2), ('cl', 3)],
+                          [('cl', 1), ('vln', 1)]])
+
+        self.assertEqual(parse_inst_list('pno(ens),cl|sax,cl(2)'),
+                         [[('cl', 2)], [('cl', 1), ('sax', 1)],
+                          [('pno','ensemble')]])
+
+    def test_get_inst_dict(self):
+        self.assertEqual(get_inst_dict('foo'), 'foo')
+        self.assertEqual(get_inst_dict('any'), 'any instrument')
+
+    def test_get_name_with_count(self):
+        self.assertEqual(get_name_with_count('oboe', 1), '1 oboe')
+
+        self.assertEqual(get_name_with_count('clarinet', 2), '2 clarinet')
+
+        self.assertEqual(get_name_with_count('flute', 'ensemble'),
+                         'flute [ensemble]')
+
+    def test_get_idf(self):
+        self.assertEqual(get_idf('oboe', 1, '1 oboe'),
+                         'oboe001::1 oboe')
+
+        self.assertEqual(get_idf('clarinet', 2, '2 clarinet'),
+                         'clarinet002::2 clarinet')
+
+        self.assertEqual(get_idf('flute', 'ensemble', 'flute [ensemble]'),
+                         'fluteensemble::flute [ensemble]')
+
+
+    def test_get_instrument_fields(self):
+        id, idf, idfwa = get_instrument_fields('ob, cl, bsn')
+
+        self.assertEqual(id, ["oboe", "clarinet", "bassoon"])
+
+        self.assertEqual(idf, ["oboe001::1 oboe",
+                               "clarinet001::1 clarinet",
+                               "bassoon001::1 bassoon"])
+
+        self.assertEqual(idfwa, ["1 oboe", "1 clarinet", "1 bassoon"])
+
+        id, idf, idfwa = get_instrument_fields('pno(ens),cl|sax,cl(2)')
+
+        self.assertEqual(id, ['clarinet', 'saxophone', 'piano'])
+
+        self.assertEqual(idf, ['clarinet002::2 clarinet',
+                               'clarinet003::3 clarinet',
+                               'saxophone001::1 saxophone',
+                               'pianoensemble::piano [ensemble]'])
+
+        self.assertEqual(idfwa, ['2 clarinet', '1 clarinet OR 1 saxophone',
+                                 'piano [ensemble]'])
+
+        id, idf, idfwa = \
+            get_instrument_fields('strings(2)|strings(3)|woodwinds(2)|woodwinds(3)')
+
+        self.assertEqual(id, ['strings', 'woodwinds'])
+
+        self.assertEqual(idf, ['string instruments002::2 string instruments',
+                               'string instruments003::3 string instruments',
+                               'woodwind instruments002::2 woodwind instruments',
+                               'woodwind instruments003::3 woodwind instruments'])
+
+        self.assertEqual(idfwa, ['2 string instruments OR 3 string instruments OR 2 woodwind instruments OR 3 woodwind instruments'])
+
+
+if __name__ == '__main__':
+    # Run the tests
+    suite = TestLoader().loadTestsFromTestCase(Test)
+    result = TextTestRunner().run(suite)
+    if result.errors or result.failures:
         sys.exit(1)
+
+    # Run the CSV validation and cleanup
+    cleanup()
